4c3f17b4843f234a069fc2fb3b457e0d1ac48164
Abdelrahman Khaled Albdelmoniem <a.khaled@oasisoft.net>
2024-09-29 14:42:02 +0300

    [add ToDo Details]

--- src/app/components/to-do-details/to-do-details.component.ts
+++ src/app/components/to-do-details/to-do-details.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToDo } from '../../Models/to-do';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToDoServices } from '../../services/to-do.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-to-do-details',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './to-do-details.component.html',
   styleUrl: './to-do-details.component.scss'
 })
-export class ToDoDetailsComponent {
+export class ToDoDetailsComponent implements OnInit{
+todo!: ToDo ;
 
+constructor
+( 
+  private route: ActivatedRoute,
+  private router: Router,
+  private ToDoServices: ToDoServices
+) {}
+
+ngOnInit()
+{
+  this.getTDoById();
+}
+
+getTDoById()
+{
+  const id = this.route.snapshot.paramMap.get('id')?? '';
+  if (id) {
+    this.ToDoServices.getTDoById(id).subscribe(todo => {
+      this.todo = todo;
+    });
+  } else {
+    console.error('No ID provided in route');
+  }
+}
+
+updateToDo(){
+if (this.todo) {
+  this.ToDoServices.updateToDo(this.todo).subscribe(() => {
+    this.router.navigate(['/todos']);
+  })
+}
+}
 }
--- src/app/components/to-do-list/to-do-list.component.ts
+++ src/app/components/to-do-list/to-do-list.component.ts
@@ -2,32 +2,62 @@ import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ToDo } from '../../Models/to-do';
 import { ToDoServices } from '../../services/to-do.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-to-do-list',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    CommonModule,
+    RouterModule
   ],
   templateUrl: './to-do-list.component.html',
   styleUrl: './to-do-list.component.scss'
 })
 export class ToDoListComponent implements OnInit {
 todos : ToDo[] = [];
-newToDo : ToDo[] = {} as ToDo[];
+newToDo : ToDo = {} as ToDo;
 
 constructor( private ToDoServices: ToDoServices)
 {}
 
-ngOnInit(){
-this.getToDos()
+ngOnInit()
+{
+  this.getToDos()
 }
 
-getToDos(){
-  this.ToDoServices.getTodos().subscribe(res => {
+getToDos()
+{
+    this.ToDoServices.getTodos().subscribe(res => {
     this.todos = res;
   })
 }
+createToDo()
+{
+  const addNewToDo = {
+    id : this.newToDo.id ,
+    title : this.newToDo.title ,
+    completed : false
+  }
+    this.newToDo = addNewToDo
+
+    this.ToDoServices.createToDo(addNewToDo).subscribe(res => {
+    this.todos.push(res)
+  })
+}
+
+deleteToDo(todoId : string) : void
+{
+  this.ToDoServices.deleteToDo(todoId).subscribe(() => {
+    this.todos = this.todos.filter(todo => todo.id !==todoId)
+  });
+}
+
+
+
+
 }
 
 
